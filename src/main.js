cb700a5a65f47808f51fb506981db302483ed8ca
Germain Riahi <sinisterglitch@gmail.com>
2016-05-08 01:57:44 +0200

    Added electron container to project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 let es6Function = (string) => string;
 
-document.write(es6Function('wow, ES6 to ES5 within a electron container, ps lisa is love you'));
+document.write(es6Function('ES6 to ES5 within a electron container'));
